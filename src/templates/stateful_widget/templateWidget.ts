70ff2ab661edeac943f2f1515cd4ee75fc6e1452
Panu Avakul <balloon2258@gmail.com>
2018-09-23 17:10:02 +0900

    Fix errors and commit vscode files

--- src/templates/stateful_widget/templateWidget.ts
+++ src/templates/stateful_widget/templateWidget.ts
@@ -1,12 +1,12 @@
 export default function getWidgetData(widgetName: string, fileName: string): string {
   return `import 'package:flutter/material.dart';
-  import './${fileName}_view.dart'
+import './${fileName}_view.dart';
+
+class ${widgetName} extends StatefulWidget {
   
-  class ${widgetName} extends StatefulWidget {
-    
-    @override
-    ${widgetName}View createState() => new ${widgetName}View();
-  }
+  @override
+  ${widgetName}View createState() => new ${widgetName}View();
+}
   
-  `;
+`;
 }
--- src/templates/stateful_widget/templateWidgetViewModel.ts
+++ src/templates/stateful_widget/templateWidgetViewModel.ts
@@ -1,9 +1,9 @@
 export default function getWidgetViewModelData(widgetName: string, fileName: string) {
   return `import 'package:flutter/material.dart';
+import './${fileName}.dart';
 
-  abstract class ${widgetName}ViewModel extends State<${widgetName}> {
-    // Add your state and logic here
-  }
-  
-  `;
+abstract class ${widgetName}ViewModel extends State<${widgetName}> {
+  // Add your state and logic here
+}
+`;
 }
